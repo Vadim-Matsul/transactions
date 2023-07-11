5d8c3ce5e4cb49e6398377af3db32b721f5055ce
Vadim Matsul <vmatsul@devvela.com>
2023-07-12 00:07:03 +0300

    T-3: add php-logic with html-render

--- app/App.php
+++ app/App.php
@@ -2,4 +2,62 @@
 
 declare(strict_types = 1);
 
-/* TODO: add code */
+function getFolderFilesPaths (string $dirName): array {
+   $files = [];
+
+   foreach (scandir($dirName) as $partialFilePath){
+       if (is_dir($partialFilePath)) continue;
+       $files[] = $dirName . $partialFilePath;
+   }
+
+   return $files;
+}
+
+function getFileContent (string $filePath, ?callable $normalizer = null): array {
+    if (!file_exists($filePath)){
+        $message = 'File: ' . "$filePath" . ' does not exist';
+        trigger_error($message, E_USER_ERROR);
+    }
+
+    $file = fopen($filePath, 'r');
+
+    $content = [];
+    while (($dataArr = fgetcsv($file)) !== false){
+        if (is_callable($normalizer)){
+            $dataArr = $normalizer($dataArr);
+        }
+
+        $content[] = $dataArr;
+    }
+
+    return $content;
+}
+
+function normalizeTransaction (array $transactionContent): array {
+    [$data, $checkNumber, $desc, $amount] = $transactionContent;
+
+    $amount = (float)str_replace(['$', ','], '', $amount);
+
+    return [
+        'date' => $data,
+        'checkNumber' => $checkNumber,
+        'desc' => $desc,
+        'amount' => $amount,
+    ];
+}
+
+function calculateTotalTransactions (array $transactions): array {
+    $totals = ['netTotal' => 0, 'totalIncome' => 0, 'totalExpense' => 0];
+
+    foreach ($transactions as $transaction){
+        $totals['netTotal'] += $transaction['amount'];
+
+        if ($transaction['amount'] > 0) {
+            $totals['totalIncome'] += $transaction['amount'];
+        } else {
+            $totals['totalExpense'] += $transaction['amount'];
+        }
+    }
+
+    return $totals;
+}
--- public/index.php
+++ public/index.php
@@ -7,3 +7,27 @@ define('FILES_PATH', $root . 'transactions_csv' . DIRECTORY_SEPARATOR);
 define('VIEWS_PATH', $root . 'views' . DIRECTORY_SEPARATOR);
 
 
+/* * */
+require APP_PATH . 'App.php';
+require APP_PATH . 'helpers.php';
+
+function getTransactions (): array {
+    $transactionFiles = getFolderFilesPaths(FILES_PATH);
+
+    $allTransactions = [];
+    foreach ($transactionFiles as $transactionFile){
+        $transactionContent = getFileContent($transactionFile, 'normalizeTransaction');
+        $allTransactions = array_merge($allTransactions, $transactionContent);
+    }
+
+    return $allTransactions;
+}
+
+$transactions = getTransactions();
+$total = calculateTotalTransactions($transactions);
+require VIEWS_PATH . 'transactions.php';
+
+
+
+
+
